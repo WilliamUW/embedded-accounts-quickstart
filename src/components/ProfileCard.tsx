424cf826a6e8a0dd3c9010f25aeca80ea6ff555b
William Wang <bwilliamwang@gmail.com>
2024-04-26 23:20:22 -0400

    Update ProfileCard.tsx

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,13 +1,23 @@
 "use client";
 
 import { useSmartAccountClient, useUser } from "@alchemy/aa-alchemy/react";
+
 import { SendUOButton } from "./SendUOButton";
+import { useEffect } from "react";
 
 export const ProfileCard = () => {
   const user = useUser();
   const { client } = useSmartAccountClient({
     type: "MultiOwnerModularAccount",
   });
+
+  function openTab() {
+    if (client?.account.address) {
+      window.open(
+        `https://monumints.vercel.app/?walletAddress=${client.account.address}`,
+      );
+    }
+  }
 
   return (
     <div className="flex flex-row rounded-lg bg-white p-10 dark:bg-[#0F172A]">
@@ -27,6 +37,14 @@ export const ProfileCard = () => {
             </div>
           </div>
         </div>
+        <button
+          className="w-full transform rounded-lg bg-red-500 p-3 font-semibold text-[#FBFDFF] transition duration-500 ease-in-out hover:scale-105 disabled:bg-[#C0D4FF] disabled:hover:scale-100 dark:disabled:bg-[#4252C5]"
+          onClick={openTab}
+        >
+          <div className="flex flex-row items-center justify-center gap-3">
+            Visit MonuMints!
+          </div>
+        </button>
         <SendUOButton />
       </div>
     </div>
